5c88e9407994f184889cc00196b2bec23feb1146
Luke Bingham <luke.bingham@itv.com>
2023-03-08 16:50:13 +0000

    LB-Website | LB CC HS YA | Website start design

--- website/routes.py
+++ website/routes.py
@@ -2,7 +2,24 @@ from flask import Blueprint, render_template
 
 routes = Blueprint("routes", __name__)
 
+# Blogs  = [
+#         {
+#             'id': 1,
+#             'title': 'blog1',
+#             'body': 'blog1body',
+#         },
+#         {
+#             'id': 2,
+#             'title': 'blog2',
+#             'body': 'blog2body',
+#         },
+#     ]
 
 @routes.route("/")
 def home():
     return render_template("home.html")
+
+@routes.route("/blogs")
+def blogs():
+    return render_template("blogs.html")
+    # return render_template("blogs.html", blogs = Blogs)

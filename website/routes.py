aee13d02969ae7056b6b1d3904ca5c433c34f416
Haaris Sheikh <haaris.sheikh@itv.com>
2023-03-15 22:39:49 +0000

    Read and Write Images from Mongo DB

--- website/routes.py
+++ website/routes.py
@@ -1,25 +1,28 @@
 from flask import Blueprint, render_template
 from website import mongo
+import gridfs
+import codecs
 
 routes = Blueprint("routes", __name__)
-
-# Blogs  = [
-#         {
-#             'id': 1,
-#             'title': 'blog1',
-#             'body': 'blog1body',
-#         },
-#         {
-#             'id': 2,
-#             'title': 'blog2',
-#             'body': 'blog2body',
-#         },
-#     ]
 
 
 @routes.route("/")
 def home():
-    print(mongo.list_database_names())
+
+    # WRITE IMAGE TO MONGO DB FROM LOCAL (Eg Upload)
+
+    # file_name = "website/dog2.png"
+    # fs = gridfs.GridFS(mongo.temp_database)
+    # with open(file_name, "rb") as f:
+    #     contents = f.read()
+    # # Now store/put the image via GridFs object.
+    # fs.put(contents, filename="dog_pic2")
+
+    # READ IMAGE FROM MONGO DB
+
+    # img = fs.find_one({"filename": "dog_pic"}).read()
+    # base64_img = codecs.encode(img, "base64")
+    # actual_img = base64_img.decode("utf-8")
     return render_template("home.html")
 
 
